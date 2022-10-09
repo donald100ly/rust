824782526e363e9aa397063d619757c107ec86e0
donald100ly <donald100@163.com>
2022-10-09 21:59:21 +0800

    guess number

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,5 +1,40 @@
+
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 fn main() {
-    println!("Hello, world!");
+    println!("猜数！");
+
+    let secret_number=rand::thread_rng().gen_range(1..=100);
+    
+    println!("神秘数字是：{}",secret_number);
+    
+    loop {
+
+    println!("猜一个数");
+
+
+    let mut guess=String::new();
+
+    io::stdin().read_line(&mut guess).expect("无法读取");
+    
+    let guess:u32=match guess.trim().parse(){
+        Ok(num)=>num,
+        Err(_)=>continue,
+    };
+    
+    println!("你猜的数字是：{}",guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less    => println!("Too small!"),
+        Ordering::Greater => println!("Too Big!"),
+        Ordering::Equal   =>{
+            println!("You win!");
+            break;
+        } 
+    }
+        
+    }
 
     
 
